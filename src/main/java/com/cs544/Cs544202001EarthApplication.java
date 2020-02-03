b0d3a2737ae758fd10197970b1effab47ecd329e
MenilikBelay <menilikbelay@gmail.com>
2020-02-03 16:39:10 -0600

    Project Starter

---
+++ src/main/java/com/cs544/Cs544202001EarthApplication.java
@@ -0,0 +1,13 @@
+package com.cs544;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs544202001EarthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs544202001EarthApplication.class, args);
+	}
+
+}
