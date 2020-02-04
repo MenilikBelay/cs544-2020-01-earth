cda11e8f71e053066072f8632fa7ccd5b33d2471
Assefa <assefamit@gamil.com>
2020-02-04 06:19:59 -0600

    some changes on Student

--- src/main/java/com/cs544/domain/Student.java
+++ src/main/java/com/cs544/domain/Student.java
@@ -1,17 +1,12 @@
 package com.cs544.domain;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
 import java.util.List;
 
 @Entity
 public class Student extends IUser {
 
-//	@Id
-//	@GeneratedValue(strategy = GenerationType.AUTO)
-//	private Long id;
 	private String studentID;
 	private String barcode;
 
