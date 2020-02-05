18b0acd4cf40520810c04525e4ba52673643e943
gfasil <fhabtegiorgis@mum.edu>
2020-02-05 14:26:02 -0600

    made some changes on CourseofferingService

--- src/main/java/com/cs544/service/CourseofferingService.java
+++ src/main/java/com/cs544/service/CourseofferingService.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 public interface CourseofferingService {
 
     public OfferedCourse add(OfferedCourse course, String id);
-    public ResponseEntity<?> deleteCourseById(String id);
+    public ResponseEntity<?> deleteCourseOfferingById(String id);
 
     public OfferedCourse update(String id,OfferedCourse offeredCourse);
     public OfferedCourse getCourseOfferingById(String id);
